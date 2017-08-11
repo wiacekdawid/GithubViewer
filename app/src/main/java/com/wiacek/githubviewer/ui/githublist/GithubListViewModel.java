a9c7c8cfb79c705d652d0e8d3af44ddf03d2eb9b
Dawid Wiącek <dawid.wiacek@rst.com.pl>
2017-08-11 07:53:25 +0200

    [ADD] Add databinding support

---
+++ app/src/main/java/com/wiacek/githubviewer/ui/githublist/GithubListViewModel.java
@@ -0,0 +1,10 @@
+package com.wiacek.githubviewer.ui.githublist;
+
+import android.databinding.BaseObservable;
+
+/**
+ * Created by wiacek.dawid@gmail.com
+ */
+
+public class GithubListViewModel extends BaseObservable {
+}
